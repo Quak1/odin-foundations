0ea38fb12468122907cb17567a763b307ea531f9
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-14 12:19:28 -0700

    Add slider to change grid size

--- 04-etch-a-sketch/script.js
+++ 04-etch-a-sketch/script.js
@@ -1,16 +1,34 @@
-const grid = document.querySelector("#grid-container");
+const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("row");
-  row.classList.add("row");
+const slider = document.querySelector("#grid-selector");
+slider.oninput = () => {
+  console.log(slider.value);
+  changeGrid(slider.value);
+};
 
-  for (let i = 0; i < 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("mouseenter", (e) =>
-      e.target.classList.add("marked")
-    );
-    row.appendChild(cell);
+function changeGrid(size) {
+  const oldGrid = document.querySelector("#grid");
+  oldGrid.remove();
+
+  const grid = document.createElement("div");
+  grid.setAttribute("id", "grid");
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("row");
+    row.classList.add("row");
+
+    for (let i = 0; i < size; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("mouseenter", (e) =>
+        e.target.classList.add("marked")
+      );
+      row.appendChild(cell);
+    }
+    grid.appendChild(row);
   }
-  grid.appendChild(row);
+
+  container.appendChild(grid);
 }
+
+changeGrid(16);
